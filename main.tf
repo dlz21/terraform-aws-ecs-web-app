3225a26b851b383b163f80a21761eee80c4e5953
Leonardo Zavala (Leo) <lezavala@torchmarkcorp.com>
2019-06-10 10:28:27 -0700

    Updates module version.

--- main.tf
+++ main.tf
@@ -214,7 +214,7 @@ resource "aws_codedeploy_deployment_group" "default" {
 
 module "ecs_bg_codepipeline" {
   enabled               = "${var.codepipeline_enabled}"
-  source                = "git::https://github.com/GMADLA/terraform-aws-ecs-codepipeline.git?ref=tags/0.10.0-dev.2"
+  source                = "git::https://github.com/GMADLA/terraform-aws-ecs-codepipeline.git?ref=tags/0.10.0-dev.3"
   name                  = "${var.name}"
   namespace             = "${var.namespace}"
   stage                 = "${var.stage}"
